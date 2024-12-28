7acbd5468b7ada1660bd061bd69855ed97e0f9fe
Mark Anthony Navarro <118058786+max31337@users.noreply.github.com>
2024-12-28 19:54:26 +0800

    Update TenantDashboard.php

--- app/Livewire/TenantDashboard.php
+++ app/Livewire/TenantDashboard.php
@@ -6,6 +6,15 @@ use Livewire\Component;
 
 class TenantDashboard extends Component
 {
+
+   public $widgets;
+   public $tenantinfo;
+   public $tenantmembers;
+
+public mount(){
+ this-> &
+}
+
     public function render()
     {
         return view('livewire.tenant-dashboard');
