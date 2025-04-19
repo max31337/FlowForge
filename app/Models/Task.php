3f11d9e98dd83b966c06d705929f14b44c3dae63
Mark Anthony Navarro <118058786+max31337@users.noreply.github.com>
2025-04-19 20:22:04 +0800

    Update Task.php

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,22 +2,40 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
 
-    protected $fillable = ['title', 'description', 'due_date', 'status', 'user_id', 'tenant_id'];
+    protected $fillable = [
+        'title',
+        'description',
+        'due_date',
+        'status',
+        'user_id',
+        'project_id',
+        'tenant_id',
+    ];
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
     public function tenant()
     {
         return $this->belongsTo(Tenant::class);
+    }
+
+    public function files()
+    {
+        return $this->hasMany(File::class);
     }
 }
