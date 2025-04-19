e0602a2973526098b5ae287544a90a794e333f65
Mark Anthony Navarro <118058786+max31337@users.noreply.github.com>
2025-04-19 20:39:57 +0800

    Create TenantService.php

---
+++ app/Services/Tenant/TenantService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Services\Tenant;
+
+use App\Models\Tenant;
+use Illuminate\Support\Collection;
+
+class TenantService implements TenantServiceInterface
+{
+    public function getAll(): Collection
+    {
+        return Tenant::all();
+    }
+
+    public function getById(int $id): ?Tenant
+    {
+        return Tenant::find($id);
+    }
+
+    public function create(array $data): Tenant
+    {
+        return Tenant::create($data);
+    }
+
+    public function update(int $id, array $data): bool
+    {
+        $tenant = Tenant::findOrFail($id);
+        return $tenant->update($data);
+    }
+
+    public function delete(int $id): bool
+    {
+        $tenant = Tenant::findOrFail($id);
+        return $tenant->delete();
+    }
+}
