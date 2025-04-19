3c2ed5ff4eefd5d1c1d6bfaf369acf6ece95719b
Mark Anthony Navarro <118058786+max31337@users.noreply.github.com>
2025-04-19 20:14:57 +0800

    Create UserRepository.php

---
+++ app/Repositories/Eloquent/UserRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\User;
+use App\Repositories\Contracts\UserRepositoryInterface;
+use Illuminate\Support\Collection;
+
+class UserRepository implements UserRepositoryInterface
+{
+    public function all(): Collection
+    {
+        return User::all();
+    }
+
+    public function find(int $id): ?User
+    {
+        return User::find($id);
+    }
+
+    public function findByEmail(string $email): ?User
+    {
+        return User::where('email', $email)->first();
+    }
+
+    public function create(array $data): User
+    {
+        return User::create($data);
+    }
+
+    public function update(User $user, array $data): bool
+    {
+        return $user->update($data);
+    }
+
+    public function delete(User $user): bool
+    {
+        return $user->delete();
+    }
+}
