2932559bbab4639c0898fb5390e6eec7cc02ba4d
Mark Anthony Navarro <118058786+max31337@users.noreply.github.com>
2025-04-19 20:31:52 +0800

    Create ProjectRepository.php

---
+++ app/Repositories/Eloquent/ProjectRepository.php
@@ -0,0 +1,18 @@
+<?php 
+namespace App\Repositories\Interfaces;
+
+use App\Models\Project;
+use Illuminate\Support\Collection;
+
+interface ProjectRepositoryInterface
+{
+    public function all(): Collection;
+
+    public function find(int $id): ?Project;
+
+    public function create(array $data): Project;
+
+    public function update(int $id, array $data): bool;
+
+    public function delete(int $id): bool;
+}
