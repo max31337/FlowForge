05c55f2aa8146babd479928d47e5b395200330f1
Mark Anthony Navarro <118058786+max31337@users.noreply.github.com>
2025-04-19 21:17:05 +0800

    Create User/UserServiceInterface.php

---
+++ app/Services/User/UserServiceInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services\Interfaces;
+
+use App\Models\User;
+
+interface UserServiceInterface
+{
+    public function getAllUsers();
+    public function getUserById(int $id): ?User;
+    public function createUser(array $data): User;
+    public function updateUser(int $id, array $data): bool;
+    public function deleteUser(int $id): bool;
+    public function getUserByEmail(string $email): ?User;
+}
