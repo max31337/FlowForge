24b68a840f6cdc150b4215f5944032a112ca352e
Mark Anthony Navarro <118058786+max31337@users.noreply.github.com>
2025-04-19 21:05:36 +0800

    Update UserRepositoryInterface.php

--- app/Repositories/User/UserRepositoryInterface.php
+++ app/Repositories/User/UserRepositoryInterface.php
@@ -1,1 +1,16 @@
-hdhsh
+<?php
+
+namespace App\Repositories\Contracts;
+
+use App\Models\User;
+use Illuminate\Support\Collection;
+
+interface UserRepositoryInterface
+{
+    public function all(): Collection;
+    public function find(int $id): ?User;
+    public function findByEmail(string $email): ?User;
+    public function create(array $data): User;
+    public function update(User $user, array $data): bool;
+    public function delete(User $user): bool;
+}
