2b294e6835275647ca5efd19189981a8bd4e2db1
Mark Anthony Navarro <118058786+max31337@users.noreply.github.com>
2025-04-19 20:07:13 +0800

    Create Repositories/Contracts/TenantRepositoryInterface.php

---
+++ app/Repositories/Contracts/TenantRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+use App\Models\Tenant;
+use Illuminate\Support\Collection;
+
+interface TenantRepositoryInterface
+{
+    public function all(): Collection;
+    public function find(int $id): ?Tenant;
+    public function findBySlug(string $slug): ?Tenant;
+    public function create(array $data): Tenant;
+    public function update(Tenant $tenant, array $data): bool;
+    public function delete(Tenant $tenant): bool;
+}
